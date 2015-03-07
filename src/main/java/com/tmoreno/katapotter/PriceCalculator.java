a81b43e83a3383d0f25148c4f09351f39ae720ec
tmoreno <tmorenobernal@yahoo.es>
2015-03-07 20:02:20 +0100

    I have written the minimum code for pass the tests

--- src/main/java/com/tmoreno/katapotter/PriceCalculator.java
+++ src/main/java/com/tmoreno/katapotter/PriceCalculator.java
@@ -4,6 +4,7 @@ public class PriceCalculator {
 
 	private static final String BASE_PRICE = "8";
 	private static final String DISCOUNT_5_PERCENT = "0.95";
+	private static final String DISCOUNT_10_PERCENT = "0.9";
 
 	public String calculate(int[] basket) {
 		if (basket.length == 0) {
@@ -13,8 +14,24 @@ public class PriceCalculator {
 		} else {
 			String result = BASE_PRICE + " * " + basket.length;
 
-			if (basket[0] != basket[1]) {
+			int differentBooks = 0;
+			for (int i = 1; i < basket.length; i++) {
+				if (basket[i] != basket[i - 1]) {
+					differentBooks++;
+				}
+			}
+
+			switch (differentBooks) {
+			case 1:
 				result += " * " + DISCOUNT_5_PERCENT;
+				break;
+
+			case 2:
+				result += " * " + DISCOUNT_10_PERCENT;
+				break;
+
+			default:
+				break;
 			}
 
 			return result;
