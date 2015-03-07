35d2136357a279086942dbb3c37da1b80ff94f75
tmoreno <tmorenobernal@yahoo.es>
2015-03-07 19:51:07 +0100

    Refactor

--- src/main/java/com/tmoreno/katapotter/PriceCalculator.java
+++ src/main/java/com/tmoreno/katapotter/PriceCalculator.java
@@ -3,6 +3,7 @@ package com.tmoreno.katapotter;
 public class PriceCalculator {
 
 	private static final String BASE_PRICE = "8";
+	private static final String DISCOUNT_5_PERCENT = "0.95";
 
 	public String calculate(int[] basket) {
 		if (basket.length == 0) {
@@ -13,7 +14,7 @@ public class PriceCalculator {
 			String result = BASE_PRICE + " * " + basket.length;
 
 			if (basket[0] != basket[1]) {
-				result += " * 0.95";
+				result += " * " + DISCOUNT_5_PERCENT;
 			}
 
 			return result;
