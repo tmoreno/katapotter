8b89ad4062857303e898138601b8a6ce4e1c296f
tmoreno <tmorenobernal@yahoo.es>
2015-03-07 19:25:39 +0100

    Refactor

--- src/test/java/com/tmoreno/katapotter/AppTest.java
+++ src/test/java/com/tmoreno/katapotter/AppTest.java
@@ -1,20 +1,26 @@
 package com.tmoreno.katapotter;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AppTest {
 
+	private PriceCalculator calculator;
+
+	@Before
+	public void setUp() {
+		calculator = new PriceCalculator();
+	}
+
 	@Test
 	public void givenEmptyBagPriceIs0() {
-		PriceCalculator calculator = new PriceCalculator();
 		String price = calculator.calculate(new int[] {});
 		Assert.assertEquals("0", price);
 	}
 
 	@Test
 	public void givenBagWithBook0PriceIs8() {
-		PriceCalculator calculator = new PriceCalculator();
 		String price = calculator.calculate(new int[] { 0 });
 		Assert.assertEquals("8", price);
 	}
