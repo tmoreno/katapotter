45f2871a6f25d6f223f32809f3bc605cad5ca2d0
tmoreno <tmorenobernal@yahoo.es>
2015-03-08 17:54:18 +0100

    Test that fails for my current code.

---
+++ src/test/java/com/tmoreno/katapotter/SeveralDiscountsTest.java
@@ -0,0 +1,25 @@
+package com.tmoreno.katapotter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SeveralDiscountsTest {
+
+	private PriceCalculator calculator;
+	private int[] basket;
+
+	@Before
+	public void setUp() {
+		calculator = new PriceCalculator();
+	}
+
+	@Test
+	public void applyOneDiscount() {
+		basket = new int[] { 0, 0, 1 };
+
+		String price = calculator.calculate(basket);
+
+		Assert.assertEquals("8 + (8 * 2 * 0.95)", price);
+	}
+}
