1273c87a9b01b9b49b79dc2ce43fac8a1adae1b2
tmoreno <tmorenobernal@yahoo.es>
2015-04-03 18:14:15 +0200

    Test that fails for my current code.

--- src/test/java/com/tmoreno/katapotter/EdgeCasesTest.java
+++ src/test/java/com/tmoreno/katapotter/EdgeCasesTest.java
@@ -23,4 +23,13 @@ public class EdgeCasesTest {
 		Assert.assertEquals("2 * (8 * 4 * 0.8)", price);
 	}
 
+	@Test
+	public void twoGroupsOf4BooksAnd3GroupsOf5BooksIsBetter() {
+		basket = new int[] { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4 };
+
+		String price = calculator.calculate(basket);
+
+		Assert.assertEquals("3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8)", price);
+	}
+
 }
